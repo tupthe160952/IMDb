a9ff08a05f805abbc723535f3c58363894fa24ed
tupthe160952 <tupthe160952@fpt.edu.vn>
2024-11-04 13:19:01 +0700

    Login + Register

--- IMDB/frontend/src/App.tsx
+++ IMDB/frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import RateStar from "./components/RateStar";
-
+import Register from './components/Register';
+import LoginForm from './components/Login';
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/footer" element={<Footer />} />
         <Route path="/rateStar" element={<RateStar />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LoginForm />} />
           
       </Routes>
     </BrowserRouter>
